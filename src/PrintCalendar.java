c236ab5bb93fbb126978ce70184580b2b74d54f5
Kevin Minseok Oh <oms3749@gmail.com>
2021-10-18 23:10:14 +0900

    Rename Print

--- src/PrintCalendar.java
+++ src/PrintCalendar.java
@@ -1,4 +1,4 @@
-public class Print {
+public class PrintCalendar {
 	public static void main(String[] args) {
 		System.out.println("일  월  화  수 목  금 토");
 		System.out.println("--------------------");
