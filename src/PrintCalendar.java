49bdb3e2346983c91af61b7e1a0440c316bba96e
Kevin Minseok Oh <oms3749@gmail.com>
2021-10-19 21:37:42 +0900

    Add LEAP_MAX_DAYS and add input prompt of year

--- src/PrintCalendar.java
+++ src/PrintCalendar.java
@@ -2,35 +2,54 @@ import java.util.Scanner;
 
 public class PrintCalendar {
 	private static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
 	}
 
 	public void printEachCalendar(int year, int month) {
 
-		System.out.printf("    <<%4d년 %3d월>>\n", year, month);
-		System.out.println(" S  M  T  W  T  F  S");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		System.out.printf("   <<%4d년 %3d월>>\n", year, month);
+		System.out.println("  S  M  T  W  T  F  S");
+		System.out.println("---------------------");
 
-		if ((month - 1) == 0 || (month - 1) == 2 || (month - 1) == 4 || (month - 1) == 6 || (month - 1) == 7
-				|| (month - 1) == 9 || (month - 1) == 11) {
+		int maxDay = getMaxDaysOfMonth(year, month);
 
-			System.out.println("29 30 31");
-			System.out.println(" ");
-
-		} else if ((month - 1) == 1) {
-			System.out.println(" ");
-
-		} else {
-			System.out.println("29 30");
-			System.out.println(" ");
-
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
+				System.out.println();
+			}
 		}
+		System.out.printf("\n\n");
+
+//		if ((month - 1) == 0 || (month - 1) == 2 || (month - 1) == 4 || (month - 1) == 6 || (month - 1) == 7
+//				|| (month - 1) == 9 || (month - 1) == 11) {
+//
+//			System.out.println("29 30 31");
+//			System.out.println(" ");
+//
+//		} else if ((month - 1) == 1) {
+//			System.out.println(" ");
+//
+//		} else {
+//			System.out.println("29 30");
+//			System.out.println(" ");
+//
+//		}
 	}
 
 }
--- src/Prompt.java
+++ src/Prompt.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
 
 public class Prompt {
-	
+
 	private final static String PROMPT = "cal> ";
-	
+
 	public void runPrompt() {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		PrintCalendar cal = new PrintCalendar();
 
 		int month = 1;
+		int year = 2021;
 
 		while (true) {
+
+			System.out.println("연도(Year)를 입력하세요.");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
 			System.out.println("달(Month)을 입력하세요. ('-1' 입력 시 종료)");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			month = scanner.nextInt();
 
 			if (month == -1) {
 				break;
 			}
 
-			if (month > 12 || month == 0 ) {
+			if (month > 12 || month == 0) {
 				continue;
 			}
 			System.out.printf("\n%d월의 달력은 아래와 같습니다.\n\n", month);
-			cal.printEachCalendar(2021, month);
+			cal.printEachCalendar(year, month);
 		}
 
 		System.out.println("End.");
 
 		scanner.close();
-		
+
 	}
 
-	
-	
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
