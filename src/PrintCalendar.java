fe753a79290245b98a10ec5d0c3c4b961ad6703c
Kevin Minseok Oh <oms3749@gmail.com>
2021-10-19 17:48:32 +0900

    Update using class method

--- src/PrintCalendar.java
+++ src/PrintCalendar.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
 
 public class PrintCalendar {
-	public static void main(String[] args) {
+	private static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int maxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
 		System.out.println("일  월  화  수 목  금 토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+
+	public static void main(String[] args) {
 
 		// 숫자를 입력받아, 해당하는 달의 최대 일수 출력
-		System.out.println("달을 입력하세요.");
+		System.out.println("달(Month)을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
+		PrintCalendar cal = new PrintCalendar();
 		int month = scanner.nextInt();
 
-		int maxDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("2021년 기준, %d월은 %d까지 있습니다.", month, maxDays[month - 1]);
+		System.out.printf("2021년 기준, %d월은 %d까지 있습니다.", month, cal.maxDaysOfMonth(month));
 
 		scanner.close();
 	}
 }
-
-//if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-//	System.out.println("31일 까지 있습니다.");
-//} else if (month == 2) {
-//	System.out.println("28일 까지 있습니다.");
-//} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-//	System.out.println("30일 까지 있습니다.");
-//}
