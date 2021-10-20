2f51a9fc7a264464fc355d59514993295024ce63
Kevin Minseok Oh <oms3749@gmail.com>
2021-10-21 00:26:16 +0900

    Update calendar to 'Gregorian' calendar

--- src/PrintCalendar.java
+++ src/PrintCalendar.java
@@ -1,8 +1,12 @@
 
 public class PrintCalendar {
+	
+	/** 최대 일수 반환*/
+	//각 월의 최대 일수 
 	private static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //윤년 
 
+	//윤년 구분
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
@@ -11,6 +15,7 @@ public class PrintCalendar {
 		}
 	}
 
+	//MAX_DAYS, LEAP_MAX_DAYS를 참조하여, 각 월의 최대 일수 반환 
 	public int getMaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
@@ -18,30 +23,91 @@ public class PrintCalendar {
 			return MAX_DAYS[month - 1];
 		}
 	}
+	
+	/** Zeller's congruence 공식 적용을 위한 메소드 
+	 * 1) makeMonthForFomula
+	 * 2) makeYearOfCentury
+	 * 3) makeZeroBasedCentury
+	 * 공식 구현 메소드 -> weekDay
+	 */
+	public int makeMonthForFomula(int month) {
+		if (month == 1) {
+			return 13;
+		}
+		if (month == 2) {
+			return 14;
+		}
+		
+		else {
+			return month;
+		}
+		
+	}
+	
+	public int makeYearOfCentury(int year, int month) {
+			if (makeMonthForFomula(month) == 13 && (year % 100) == 0 || makeMonthForFomula(month) == 14 && (year % 100) == 0) {
+				return 99;
+			}
+			if (makeMonthForFomula(month) == 13 || makeMonthForFomula(month) == 14) {
+				return (year % 100) - 1;
+			}
+			else {
+				return year % 100;
+			}
+		
+	}
+	public int makeZeroBasedCentury(int year, int month) {
+		
+		if (makeMonthForFomula(month) == 13 || makeMonthForFomula(month) == 14) {
+			return (year / 100) - 1;
+		}
+		else {
+			return year / 100;
+		}
+		
+	}
 
-	public void printEachCalendar(int year, int month, int weekday) {
+	/** Zeller's congruence 공식 적용*/
+	public int weekDay(int year, int month, int day) {
+				
+				int m = makeMonthForFomula(month);
+				int K = makeYearOfCentury(year, month);
+				int J = makeZeroBasedCentury(year, month);
+				
+				
+			return (day + ((13*(m+1))/5) + K + (K/4) + (J/4) + (J*5)) % 7;
+
+				
+			}
+	
+	/**캘린더 출력*/ 
+	public void printEachCalendar(int year, int month, int day) {
 
 		// Default space
 		System.out.printf("   <<%4d년 %3d월>>\n", year, month);
-		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println(" SA SU MO TU WE TH FR");
 		System.out.println("---------------------");
 
-		// print blank space of first line
+		
+		int weekday = weekDay(year, month, day); 
+		
+		
+		// 시작 요일을 받아, 그에 상응하는 첫 줄 공백 출력  
 		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
 
-		int maxDay = getMaxDaysOfMonth(year, month);
+		int maxDay = getMaxDaysOfMonth(year, month); //연도, 월을 받아 최대 일수 반환 
 		int count = 7 - weekday;
 		int delim = (count < 7) ? count : 0;
 
-		// print days of first line
+		// 첫 줄 일수 출력 
 		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
 		}
 		System.out.println();
 
-		// print from second line to last
+		// 둘째 줄부터 마지막 줄까지 출력 
 		count++;
 		for (int i = count; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
--- src/Prompt.java
+++ src/Prompt.java
@@ -1,27 +1,6 @@
 import java.util.Scanner;
 
 public class Prompt {
-	/**
-	 * week = 요일명 return 0~ 6 (0 = Sunday, 6 = Saturday)
-	 */
-	public int parseDay(String week) {
-		if (week.equals("su"))
-			return 0;
-		else if (week.equals("mo"))
-			return 1;
-		else if (week.equals("tu"))
-			return 2;
-		else if (week.equals("we"))
-			return 3;
-		else if (week.equals("th"))
-			return 4;
-		else if (week.equals("fr"))
-			return 5;
-		else if (week.equals("sa"))
-			return 6;
-		else
-			return 0;
-	}
 
 	public void runPrompt() {
 
@@ -30,10 +9,11 @@ public class Prompt {
 
 		int month = 1;
 		int year = 2021;
-		int weekday = 0;
+		int day = 1;
+
 		while (true) {
 
-			System.out.println("연도(Year)를 입력하세요. ('-1' 입력 시 종료)");
+			System.out.println("출력하고자 하는 달력의 연도(Year)를 입력하세요. ('-1' 입력 시 종료)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
 			if (year == -1)
@@ -42,18 +22,14 @@ public class Prompt {
 			System.out.println("달(Month)을 입력하세요. ");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			System.out.println("첫째 날의 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
 
-			System.out.print("DAY> ");
 
 			if (month > 12 || month < 1) {
 				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
-			System.out.printf("\n%d월의 달력은 아래와 같습니다.\n\n", month);
-			cal.printEachCalendar(year, month, weekday);
+			System.out.printf("\n %4d년 %d월의 달력은 아래와 같습니다.\n\n", year, month);
+			cal.printEachCalendar(year, month, day);
 		}
 
 		System.out.println("End.");
