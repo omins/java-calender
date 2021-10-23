78229bb203133081b0aa1839e8e9abe76ec07adc
Kevin Minseok Oh <oms3749@gmail.com>
2021-10-23 21:47:56 +0900

    Implement 'add and search events' function

--- src/PrintCalendar.java
+++ src/PrintCalendar.java
@@ -1,12 +1,23 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 public class PrintCalendar {
-	
-	/** 최대 일수 반환*/
-	//각 월의 최대 일수 
-	private static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //윤년 
 
-	//윤년 구분
+	/** 캘린더 일정 등록을 위한 HashMap */
+	private HashMap<Date, String> planMap;
+
+	/** 윤년, 평년의 각월 최대 일수 배열 */
+	private static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 윤년
+
+	// HashMap 메모리 할당
+	public PrintCalendar() {
+		planMap = new HashMap<Date, String>();
+	}
+
+	// 윤년 구분
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
@@ -15,7 +26,7 @@ public class PrintCalendar {
 		}
 	}
 
-	//MAX_DAYS, LEAP_MAX_DAYS를 참조하여, 각 월의 최대 일수 반환 
+	// MAX_DAYS, LEAP_MAX_DAYS를 참조하여, 각 월의 최대 일수 반환
 	public int getMaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
@@ -23,12 +34,34 @@ public class PrintCalendar {
 			return MAX_DAYS[month - 1];
 		}
 	}
-	
-	/** Zeller's congruence 공식 적용을 위한 메소드 
-	 * 1) makeMonthForFomula
+
+	/**
+	 * 일정 등록 및 열람
+	 * 
+	 * @param date ex: "2021-10-23"
+	 * @param plan
+	 */
+
+	// 일정 등록
+	public void registerPlan(String strDate, String plan) throws ParseException {
+
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		planMap.put(date, plan);
+	}
+
+	// 일정 검색
+	public String searchPlan(String strDate) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		String plan = planMap.get(date);
+		return plan;
+	}
+
+	/**
+	 * 정확한 달력 출력하기 위한 Zeller's congruence 공식 관련 메소드 
+	 * 1) makeMonthForFomula 
 	 * 2) makeYearOfCentury
-	 * 3) makeZeroBasedCentury
-	 * 공식 구현 메소드 -> weekDay
+	 * 3) makeZeroBasedCentury 
+	 * 공식 적용 메소드 -> weekDay
 	 */
 	public int makeMonthForFomula(int month) {
 		if (month == 1) {
@@ -37,50 +70,48 @@ public class PrintCalendar {
 		if (month == 2) {
 			return 14;
 		}
-		
+
 		else {
 			return month;
 		}
-		
+
 	}
-	
+
 	public int makeYearOfCentury(int year, int month) {
-			if (makeMonthForFomula(month) == 13 && (year % 100) == 0 || makeMonthForFomula(month) == 14 && (year % 100) == 0) {
-				return 99;
-			}
-			if (makeMonthForFomula(month) == 13 || makeMonthForFomula(month) == 14) {
-				return (year % 100) - 1;
-			}
-			else {
-				return year % 100;
-			}
-		
+		if (makeMonthForFomula(month) == 13 && (year % 100) == 0
+				|| makeMonthForFomula(month) == 14 && (year % 100) == 0) {
+			return 99;
+		}
+		if (makeMonthForFomula(month) == 13 || makeMonthForFomula(month) == 14) {
+			return (year % 100) - 1;
+		} else {
+			return year % 100;
+		}
+
 	}
+
 	public int makeZeroBasedCentury(int year, int month) {
-		
+
 		if (makeMonthForFomula(month) == 13 || makeMonthForFomula(month) == 14) {
 			return (year / 100) - 1;
-		}
-		else {
+		} else {
 			return year / 100;
 		}
-		
+
 	}
 
-	/** Zeller's congruence 공식 적용*/
+	// Zeller's congruence 공식 적용
 	public int weekDay(int year, int month, int day) {
-				
-				int m = makeMonthForFomula(month);
-				int K = makeYearOfCentury(year, month);
-				int J = makeZeroBasedCentury(year, month);
-				
-				
-			return (day + ((13*(m+1))/5) + K + (K/4) + (J/4) + (J*5)) % 7;
 
-				
-			}
-	
-	/**캘린더 출력*/ 
+		int m = makeMonthForFomula(month);
+		int K = makeYearOfCentury(year, month);
+		int J = makeZeroBasedCentury(year, month);
+
+		return (day + ((13 * (m + 1)) / 5) + K + (K / 4) + (J / 4) + (J * 5)) % 7;
+
+	}
+
+	/** 캘린더 출력 */
 	public void printEachCalendar(int year, int month, int day) {
 
 		// Default space
@@ -88,26 +119,24 @@ public class PrintCalendar {
 		System.out.println(" SA SU MO TU WE TH FR");
 		System.out.println("---------------------");
 
-		
-		int weekday = weekDay(year, month, day); 
-		
-		
-		// 시작 요일을 받아, 그에 상응하는 첫 줄 공백 출력  
+		int weekday = weekDay(year, month, day);
+
+		// 시작 요일을 받아, 그에 상응하는 첫 줄 공백 출력
 		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
 
-		int maxDay = getMaxDaysOfMonth(year, month); //연도, 월을 받아 최대 일수 반환 
+		int maxDay = getMaxDaysOfMonth(year, month); // 연도, 월을 받아 최대 일수 반환
 		int count = 7 - weekday;
 		int delim = (count < 7) ? count : 0;
 
-		// 첫 줄 일수 출력 
+		// 첫 줄 일수 출력
 		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
 		}
 		System.out.println();
 
-		// 둘째 줄부터 마지막 줄까지 출력 
+		// 둘째 줄부터 마지막 줄까지 출력
 		count++;
 		for (int i = count; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
@@ -116,6 +145,14 @@ public class PrintCalendar {
 			}
 		}
 		System.out.printf("\n\n");
+	}
+
+	// Test code
+	public static void main(String[] args) throws ParseException {
+		PrintCalendar cal = new PrintCalendar();
+		cal.registerPlan("2021-10-23", "코딩하는 날!");
+		System.out.println(cal.searchPlan("2021-10-23").equals("코딩하는 날!"));
+
 	}
 
 }
--- src/Prompt.java
+++ src/Prompt.java
@@ -1,45 +1,100 @@
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
 
-	public void runPrompt() {
+	public void printMenu() {
+
+		System.out.printf("    << 달력 프로그램>>    \n");
+		System.out.println("+--------------------+");
+		System.out.println("| 1. 일정 등록 ");
+		System.out.println("| 2. 일정 검색 ");
+		System.out.println("| 3. 달력 보기 ");
+		System.out.println("| h. 도움말 보기 q. 종료 ");
+		System.out.println("+--------------------+");
+		System.out.printf("\n\n");
+
+	}
+
+	public void runPrompt() throws ParseException {
+		printMenu();
 
 		Scanner scanner = new Scanner(System.in);
 		PrintCalendar cal = new PrintCalendar();
+
+		while (true) {
+			System.out.println("명령 : 1, 2, 3, h, q");
+			String cmd = scanner.next();
+			if (cmd.equals("1"))
+				cmdRegister(scanner, cal);
+			else if (cmd.equals("2"))
+				cmdSearch(scanner, cal);
+			else if (cmd.equals("3"))
+				cmdCal(scanner, cal);
+			else if (cmd.equals("h"))
+				printMenu();
+			else if (cmd.equals("q"))
+				break;
+		}
+
+		System.out.println("End.");
+		scanner.close();
+
+	}
+
+	private void cmdCal(Scanner s, PrintCalendar c) { // scanner와 calendar는 참조호출하여 사용
+
+		// TODO Auto-generated method stub
 
 		int month = 1;
 		int year = 2021;
 		int day = 1;
 
-		while (true) {
+		System.out.println("출력하고자 하는 달력의 연도(Year)를 입력하세요.");
+		System.out.print("YEAR> ");
+		year = s.nextInt();
 
-			System.out.println("출력하고자 하는 달력의 연도(Year)를 입력하세요. ('-1' 입력 시 종료)");
-			System.out.print("YEAR> ");
-			year = scanner.nextInt();
-			if (year == -1)
-				break;
+		System.out.println("달(Month)을 입력하세요. ");
+		System.out.print("MONTH> ");
+		month = s.nextInt();
 
-			System.out.println("달(Month)을 입력하세요. ");
-			System.out.print("MONTH> ");
-			month = scanner.nextInt();
-
-
-			if (month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
-			}
-			System.out.printf("\n %4d년 %d월의 달력은 아래와 같습니다.\n\n", year, month);
-			cal.printEachCalendar(year, month, day);
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
 		}
+		System.out.printf("\n %4d년 %d월의 달력은 아래와 같습니다.\n\n", year, month);
+		c.printEachCalendar(year, month, day);
+	}
 
-		System.out.println("End.");
+	private void cmdSearch(Scanner s, PrintCalendar c) throws ParseException {
+		System.out.printf("\n[일정 검색]\n");
+		System.out.println("날짜를 입력해주세요. ('yyyy-MM-dd')");
+		String date = s.next();
+		String plan = c.searchPlan(date);
+		if (plan != null && plan.length() != 0) {
+			System.out.printf("\n%s의 일정: %s\n\n", date, plan);
+		} else {
+			System.out.printf("등록된 일정이 없습니다.\n\n");
+		}
+	}
 
-		scanner.close();
+	private void cmdRegister(Scanner s, PrintCalendar c) throws ParseException {
+		System.out.printf("\n[새 일정 등록]\n");
+		System.out.printf("날짜를 입력해주세요. ('yyyy-MM-dd')\n");
+		String date = s.next();
+		String text = "";
+
+		s.nextLine(); // ignore one newline
+		System.out.printf("\n일정을 입력해 주세요.\n");
+		text = s.nextLine();
+
+		c.registerPlan(date, text);
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Prompt p = new Prompt();
+
 		p.runPrompt();
 	}
 }
